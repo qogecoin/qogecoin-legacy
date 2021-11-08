27c86253388b18590dc1571a4802a586a33a82a3
qogecoin <88919029+qogecoin-dev@users.noreply.github.com>
2021-11-08 22:41:55 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -15,24 +15,23 @@ install_dependencies() {
        libtool \
        pkg-config \
        python3
- 
-# Build berkeley-db-4.8.3 from source  
-  wget http://download.oracle.com/berkeley-db/db-4.8.30.zip  
+
+# Build berkeley-db-4.8.3 from source.
+  wget http://download.oracle.com/berkeley-db/db-4.8.30.zip
   checksum="4f538b56681a871cc71658ed9a6120081b74b474eaa73ba2c958abea04cc98ce"
     if ! echo "$checksum db-4.8.30.zip" | sha256sum -c -; then
       echo "Checksum failed" >&2
     exit 1
-  fi 
-  unzip db-4.8.30.zip
-  cd db-4.8.30
+  fi
+  unzip db-4.8.30.zip && cd db-4.8.30
   sudo sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' dbinc/atomic.h
   cd build_unix/
   ../dist/configure --prefix=/usr/local --enable-cxx
-  make
+  make -j$(nproc)
   sudo make install
   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
   
-# Gui dependencies   
+# Gui dependencies.
   sudo apt install -y \
        libprotobuf-dev \
        libqt5core5a \
